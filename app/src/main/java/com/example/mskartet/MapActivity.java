6ac1e9c07f0b9de4fa95acfa626d7b02a51b6d63
Glenn <glenn.nordmarken@gmail.com>
2023-05-07 01:21:40 +0200

    bra

--- app/src/main/java/com/example/mskartet/MapActivity.java
+++ app/src/main/java/com/example/mskartet/MapActivity.java
@@ -33,6 +33,91 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
                 .position(lunner)
                 .title("Lunner Motorsport"));
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(lunner));
+
+        LatLng jevnaker = new LatLng(60.228075, 10.338379);
+        googleMap.addMarker(new MarkerOptions()
+                .position(jevnaker)
+                .title("Jevnaker Motorklubb"));
+
+        LatLng gjerdrum = new LatLng(60.064787, 11.008858);
+        googleMap.addMarker(new MarkerOptions()
+                .position(gjerdrum)
+                .title("Gjerdrum Motorklubb"));
+
+        LatLng enebakk = new LatLng(59.730706, 11.144231);
+        googleMap.addMarker(new MarkerOptions()
+                .position(enebakk)
+                .title("Enebakk Motorsportklubb"));
+
+        LatLng bunes = new LatLng(59.665959, 11.761191);
+        googleMap.addMarker(new MarkerOptions()
+                .position(bunes)
+                .title("Bunes Idrettslaget"));
+
+        LatLng lier = new LatLng(59.800574, 10.287997);
+        googleMap.addMarker(new MarkerOptions()
+                .position(lier)
+                .title("Lier Motorsportklubb"));
+
+        LatLng gardermoen = new LatLng(60.188589, 11.135093);
+        googleMap.addMarker(new MarkerOptions()
+                .position(gardermoen)
+                .title("Gardermoen Motorcrossbane"));
+
+        LatLng kongsvinger = new LatLng(60.247428, 12.125493);
+        googleMap.addMarker(new MarkerOptions()
+                .position(kongsvinger)
+                .title("Kongsvinger Motorklubb"));
+
+        LatLng haslemoen = new LatLng(60.655043, 11.879655);
+        googleMap.addMarker(new MarkerOptions()
+                .position(haslemoen)
+                .title("Haslemoen Motorcrossbane"));
+
+        LatLng starmoen = new LatLng(60.863302, 11.685858);
+        googleMap.addMarker(new MarkerOptions()
+                .position(starmoen)
+                .title("Starmoen Motorsenter"));
+
+        LatLng bøverlund = new LatLng(61.044277, 10.832051);
+        googleMap.addMarker(new MarkerOptions()
+                .position(bøverlund)
+                .title("Bøverlund Motorcrossbane"));
+
+        LatLng ål = new LatLng(60.642294, 8.600979);
+        googleMap.addMarker(new MarkerOptions()
+                .position(ål)
+                .title("Ål MotorCrossbane"));
+
+        LatLng fuglehaugen = new LatLng(60.798471, 8.772668);
+        googleMap.addMarker(new MarkerOptions()
+                .position(fuglehaugen)
+                .title("Fuglehaugen MotorCrossbane"));
+
+        LatLng valdres = new LatLng(61.093315, 9.026538);
+        googleMap.addMarker(new MarkerOptions()
+                .position(valdres)
+                .title("Valdres MotorCross"));
+
+        LatLng storelvdal = new LatLng(61.598554, 10.994851);
+        googleMap.addMarker(new MarkerOptions()
+                .position(storelvdal)
+                .title("Stor-Elvdal MotorCross"));
+
+        LatLng nmktrysil = new LatLng(61.231750, 12.267718);
+        googleMap.addMarker(new MarkerOptions()
+                .position(nmktrysil)
+                .title("NMK Trysil MotorCross & Bilcross"));
+
+        LatLng slettholen = new LatLng(60.452830, 11.442625);
+        googleMap.addMarker(new MarkerOptions()
+                .position(slettholen)
+                .title("Slettholen Motorcross"));
+
+        LatLng magnor = new LatLng(59.952597, 12.176493);
+        googleMap.addMarker(new MarkerOptions()
+                .position(magnor)
+                .title("Magnor Motorcross"));
     }
 
     public void startActivityMain(View view) {
