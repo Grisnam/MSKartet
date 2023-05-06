8b46b11e0adceeff74a6cd1baa149307a2a47697
Glenn <glenn.nordmarken@gmail.com>
2023-05-06 22:35:31 +0200

    bra

--- app/src/main/java/com/example/mskartet/MainActivity.java
+++ app/src/main/java/com/example/mskartet/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_map);
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.maps);
         mapFragment.getMapAsync(this);
