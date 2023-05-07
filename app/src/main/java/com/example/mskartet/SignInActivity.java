184671bee1876bc7b8777d91a4d021d965a1911f
Glenn <glenn.nordmarken@gmail.com>
2023-05-07 15:51:05 +0200

    bra

--- app/src/main/java/com/example/mskartet/SignInActivity.java
+++ app/src/main/java/com/example/mskartet/SignInActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.content.Intent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -26,13 +27,20 @@ public class SignInActivity extends AppCompatActivity {
         loginbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // Declare the intent variable outside the if statement
+                Intent intent;
 
                 if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin")){
                     //correct
                     Toast.makeText(SignInActivity.this,"LOGIN SUCCESSFUL",Toast.LENGTH_SHORT).show();
-                }else
+
+                    // Assign the intent a value inside the if statement
+                    intent = new Intent(SignInActivity.this, WeekActivity.class);
+                    startActivity(intent);
+                } else {
                     //incorrect
                     Toast.makeText(SignInActivity.this,"LOGIN FAILED !!!",Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
