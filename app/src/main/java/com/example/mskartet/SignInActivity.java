ccd8be98962608ca4a9b4e9ea8fbdddedd29e348
Glenn <glenn.nordmarken@gmail.com>
2023-05-07 03:14:04 +0200

    bra

--- app/src/main/java/com/example/mskartet/SignInActivity.java
+++ app/src/main/java/com/example/mskartet/SignInActivity.java
@@ -1,38 +1,42 @@
 package com.example.mskartet;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.button.MaterialButton;
 
-public class SignInActivity extends MainActivity {
+public class SignInActivity extends AppCompatActivity {
 
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
 
+        TextView username =(TextView) findViewById(R.id.username);
+        TextView password =(TextView) findViewById(R.id.password);
 
-        TextView BrukerNavn =(TextView) findViewById(R.id.BrukerNavn);
-        TextView Passord =(TextView) findViewById(R.id.Passord);
+        MaterialButton loginbtn = (MaterialButton) findViewById(R.id.loginbtn);
 
-        MaterialButton logginnbtn = (MaterialButton) findViewById(R.id.logginnbtn);
-        //admin admin
-        logginnbtn.setOnClickListener(new View.OnClickListener() {
+        //admin and admin
+
+        loginbtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v)
-            {if(BrukerNavn.getText() .toString().equals("admin") && Passord.getText().toString().equals("admin")){
-                //correct
-                Toast.makeText(MainActivity.this,"LOGIN SUCCESSFUL",Toast.LENGTH_SHORT).show();
-            }else
-                //incorrect
-                Toast.makeText(MainActivity.this,"LOGIN FAILED !!!",Toast.LENGTH_SHORT).show();
-            }
+            public void onClick(View v) {
 
+                if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin")){
+                    //correct
+                    Toast.makeText(SignInActivity.this,"LOGIN SUCCESSFUL",Toast.LENGTH_SHORT).show();
+                }else
+                    //incorrect
+                    Toast.makeText(SignInActivity.this,"LOGIN FAILED !!!",Toast.LENGTH_SHORT).show();
+            }
         });
     }
-    }
+
+
+}
+
 
 
