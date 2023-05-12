4082141a618f31e56e9ff04d5182d96c34e46211
Glenn <glenn.nordmarken@gmail.com>
2023-05-12 16:47:38 +0200

    bra

--- app/src/main/java/com/example/mskartet/DescriptionActivity.java
+++ app/src/main/java/com/example/mskartet/DescriptionActivity.java
@@ -1,6 +1,5 @@
 package com.example.mskartet;
 
-import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,7 +7,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.TimePicker;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
